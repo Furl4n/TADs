eb7f9abeb8b39fd1c767a00b3ed8c626f8503db6
Pedro Furlan <pedrogfurlan1@gmail.com>
2025-07-22 17:08:19 -0300

    lista ordenada

--- TAD_Lista_Ord/TAD_Lista_Ord.c
+++ TAD_Lista_Ord/TAD_Lista_Ord.c
@@ -1,7 +1,7 @@
 #include<stdlib.h>
 #include<string.h>
 #include "TAD_Lista_Ord.h"
-#define max_vet 15
+#define max_vet 2
 #define max_str 21
 
 struct bebida{
@@ -11,7 +11,7 @@ struct bebida{
 };
 
 struct TAD_Lista_Ord{
-    struct bebida pos[max_str];
+    struct bebida pos[max_vet];
     int fim;
 };
 
@@ -33,18 +33,63 @@ int lista_cheia(lista list){
     return 0;
 }
 
-int insere_ord(lista list, bebida elem){  //adicionar diferenciação por volume caso ==
-    if(list==NULL || elem==NULL || lista_cheia(list)) return 0;
+int insere_ord(lista list, char nome[max_str], int volume, float preco){
+    if(list==NULL || lista_cheia(list)) return 0;
 
-    if(lista_vazia(list)==1 || strcmp(elem->nome, list->pos[list->fim-1].nome)==0){
-    
-        list->pos[list->fim]= *elem;
+    struct bebida elem;
+    strcpy(elem.nome, nome);
+    elem.volume = volume;
+    elem.preco = preco;
+
+    if(lista_vazia(list)==1 || strcmp(elem.nome, list->pos[list->fim-1].nome)==1) 
+    list->pos[list->fim]= elem;
+
+    else if (strcmp(elem.nome, list->pos[list->fim-1].nome) == 0) {
+        if (elem.volume >= list->pos[list->fim-1].volume) {
+            list->pos[list->fim] = elem;
+        } else {
+            int i = list->fim - 1;
+            while (i >= 0 && strcmp(elem.nome, list->pos[i].nome) == 0 && elem.volume < list->pos[i].volume) {
+                list->pos[i+1] = list->pos[i];
+                i--;
+            }
+            list->pos[i+1] = elem;
+        }
+    } 
+    else{
+        int i = list->fim - 1;
+        while (i >= 0 && (strcmp(elem.nome, list->pos[i].nome) < 0 || (strcmp(elem.nome, list->pos[i].nome) == 0 && elem.volume < list->pos[i].volume))) {
+
+        list->pos[i+1] = list->pos[i];
+        i--;
+        }
+
+        list->pos[i+1] = elem;
     }
+    
+    list->fim++;
+    return 1;
+}
 
-    int aux=0; 
 
-    while (strcmp(list->pos[aux].nome, elem->nome)==-1){
-        //adicionar codigo para mover código
+int remove_ord(lista list, char nome[max_str]){
+    if(list==NULL || lista_vazia(list) || strcmp(list->pos[0].nome, nome)==1 || strcmp(nome, list->pos[list->fim-1].nome)==1)
+    return 0;
+
+    else{
+        int aux=0;
+
+        while(strcmp(list->pos[aux].nome, nome)!=0 && aux<list->fim) aux++;
+
+        if(aux==list->fim){
+            return 0;
+        }
+
+        for(int i=aux; i<list->fim-1; i++){
+            list->pos[i]=list->pos[i+1];
+        }
     }
     
+    list->fim--;
+    return 1;    
 }
--- TAD_Lista_Ord/TAD_Lista_Ord.h
+++ TAD_Lista_Ord/TAD_Lista_Ord.h
@@ -1,7 +1,12 @@
 #define TAD_LISTA_ORD_H
-#define max_vet 15
+#define max_vet 2
 #define max_str 21
 
 typedef struct bebida* bebida;
 typedef struct TAD_Lista_Ord* lista;
 
+lista cria_lista();
+int lista_vazia(lista list);
+int lista_cheia(lista list);
+int insere_ord(lista list, char nome[max_str], int volume, float preco);
+int remove_ord(lista list, char nome[max_str]);
